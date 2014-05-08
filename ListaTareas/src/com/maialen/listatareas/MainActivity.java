77a4f095a9a82dba1075e94609ef7f54d00673c3
Maialen <Maialens22@gmail.com>
2014-05-08 12:46:03 +0200

    Primer adapter con un listado

---
+++ ListaTareas/src/com/maialen/listatareas/MainActivity.java
@@ -0,0 +1,81 @@
+package com.maialen.listatareas;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	private ArrayList<String> listado;	
+	private ArrayAdapter adapter;
+	
+	private EditText txtNuevaTarea;
+	private ListView listaTareas;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		this.listado = new ArrayList<String>();
+		
+		//contexto: entorno en el que se ejecuta
+		//el tipo de layout a rellenar
+		//datos para rellenar
+		this.adapter= new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.listado);
+		
+		this.txtNuevaTarea = (EditText) findViewById(R.id.tareaNueva);
+		this.listaTareas = (ListView) findViewById(R.id.listaTareas);
+		//decirle a la lista cual es su adaptador
+		this.listaTareas.setAdapter(this.adapter);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void anadir(View v){
+		
+		String men= String.valueOf( this.txtNuevaTarea.getText());
+		
+		if(men.length()>0){
+			this.listado.add(men);
+		}
+		this.txtNuevaTarea.setText("");
+		
+		this.adapter.notifyDataSetChanged();
+		
+	}
+	
+	
+
+}
