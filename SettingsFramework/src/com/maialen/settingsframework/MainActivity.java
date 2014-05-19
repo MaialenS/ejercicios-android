486b84f7464bec4976c1e972d3feb4135da40e31
Maialen <Maialens22@gmail.com>
2014-05-19 09:49:33 +0200

    Con acceso a las preferencias

--- SettingsFramework/src/com/maialen/settingsframework/MainActivity.java
+++ SettingsFramework/src/com/maialen/settingsframework/MainActivity.java
@@ -1,26 +1,37 @@
 package com.maialen.settingsframework;
 
+
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
+import android.preference.PreferenceManager;
 
 public class MainActivity extends Activity {
 
 	private static final int SHOW_PREFERENCES = 1;
+	
+	private TextView txtAutorefresh, txtIntervalo, txtMagnitud;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		txtAutorefresh = (TextView) findViewById(R.id.textAutorefresh);
+		txtIntervalo = (TextView) findViewById(R.id.textIntervalo);
+		txtMagnitud = (TextView) findViewById(R.id.textMagnitud);
 		
 	}  
 
@@ -47,5 +58,27 @@ public class MainActivity extends Activity {
 	}
 
 
+	
+	@Override
+	public void onResume() {
+	    super.onResume();  // Always call the superclass method first
+
+	  //mirar las preferencias
+	    Context context = getApplicationContext();
+	    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);	
+	  	//String syncConnPref = sharedPref.getString(SettingsActivity.KEY_PREF_SYNC_CONN, "");	
+	    
+	    boolean auto = prefs.getBoolean(PreferenciasActivity.KEY_PREF_ACTUALIZAR, false);
+	    
+	    String intervalo = prefs.getString(PreferenciasActivity.KEY_PREF_INTERVALOS, "default");
+	    
+	    String magnitud = prefs.getString(PreferenciasActivity.KEY_PREF_MAGNITUD, "default");
+	    
+	    txtAutorefresh.setText(Boolean.toString(auto));
+	    txtIntervalo.setText(intervalo);
+	    txtMagnitud.setText(magnitud);
+	    
+	    
+	}
 
 }
--- SettingsFramework/src/com/maialen/settingsframework/PreferenciasActivity.java
+++ SettingsFramework/src/com/maialen/settingsframework/PreferenciasActivity.java
@@ -6,6 +6,10 @@ import android.preference.PreferenceActivity;
 
 public class PreferenciasActivity extends  PreferenceActivity{
 
+	public static final String KEY_PREF_ACTUALIZAR = "AUTOREFRESH";
+	public static final String KEY_PREF_INTERVALOS = "INTERVALOS";
+	public static final String KEY_PREF_MAGNITUD = "MAGNITUD";
+	
 	public void onBuildHeaders(List<Header> target){
 		//cargar las cabeceras de las preferencias
 		loadHeadersFromResource(R.xml.userpreferenceheaders, target);
