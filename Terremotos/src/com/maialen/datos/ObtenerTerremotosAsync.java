2d4f50ad3a35753711dc3102183f5c9e9e7293bd
Maialen <Maialens22@gmail.com>
2014-05-22 11:47:27 +0200

    Commit

---
+++ Terremotos/src/com/maialen/datos/ObtenerTerremotosAsync.java
@@ -0,0 +1,35 @@
+package com.maialen.datos;
+
+import java.net.URL;
+
+import android.os.AsyncTask;
+
+public class ObtenerTerremotosAsync extends AsyncTask<URL, Integer, Long>{
+	
+	public ObtenerTerremotosAsync(){
+		
+	}
+
+	@Override
+	protected Long doInBackground(URL... arg0) {
+		// TODO Auto-generated method stub
+		return null;
+		
+		
+	}
+
+	@Override
+	protected void onProgressUpdate(Integer... progress) {
+        
+    }
+
+	@Override
+    protected void onPostExecute(Long result) {
+        
+    }
+	
+	@Override
+	protected void onPreExecute (){
+		
+	}
+}
