34ba2723b30ed691f985d98b2c5d2c121418fa0c
Maialen <Maialens22@gmail.com>
2014-05-27 13:27:27 +0200

    commit

--- TerremotosContent/src/com/maialen/terremotos/FragmentDetalleTerremoto.java
+++ TerremotosContent/src/com/maialen/terremotos/FragmentDetalleTerremoto.java
@@ -2,10 +2,16 @@ package com.maialen.terremotos;
 
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.maialen.datos.TerremotosContentProvider;
 
 import android.app.Fragment;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
@@ -13,16 +19,43 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 
 
 
-public class FragmentDetalleTerremoto extends Fragment {
+public class FragmentDetalleTerremoto extends Fragment implements LoaderCallbacks<Cursor>{
 
 	private static final String TAG = "Terremotos";
-
+	private static final int LOADER_DETALLE = 1;
 	
+	private SimpleCursorAdapter simpleAdapter;
+	
+	private SimpleCursorAdapter.ViewBinder fechaViewBlinder= new SimpleCursorAdapter.ViewBinder(){
 
+		@Override
+		public boolean setViewValue(View view, Cursor cursor,
+				int columnIndex) {
+			// TODO Auto-generated method stub
+			int esFecha = cursor
+					.getColumnIndexOrThrow(TerremotosContentProvider.TIME_COLUMN);
+			if (columnIndex == esFecha) {
+				// formatear la fecha
+				TextView textFecha = (TextView) view;
+				long fechaLong = cursor.getLong(cursor
+						.getColumnIndex(TerremotosContentProvider.TIME_COLUMN));
+				String fechaFormateada = new SimpleDateFormat(
+						"yyyy-MM-dd HH:mm:ss").format(new Date(fechaLong));
+				textFecha.setText(fechaFormateada);
+				return true;
+
+			}
+
+			return false;
+		}
+		
+	};
 
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -50,6 +83,14 @@ public class FragmentDetalleTerremoto extends Fragment {
 		    Log.d(TAG, "id terremoto-> "+id_terremoto);
 	    }
 		
+	    
+	    
+	    simpleAdapter = new SimpleCursorAdapter(getActivity(),
+				R.layout.tupla_terremoto, null, from, to, 0);
+
+		simpleAdapter.setViewBinder(fechaViewBlinder);
+		// si no se hace aqui aparece el loading
+		
 		
 		
 		
@@ -66,6 +107,8 @@ public class FragmentDetalleTerremoto extends Fragment {
 		//para poner el loader de los datos
 		//getLoaderManager().initLoader(LOADER_TERREMOTOS, null, this);
 
+		getLoaderManager().initLoader(LOADER_DETALLE, null, this);
+		
 	}
 	
 	@Override
@@ -81,6 +124,30 @@ public class FragmentDetalleTerremoto extends Fragment {
 		super.onStart();
 		// Apply any required UI change now that the Fragment is visible.
 
+	}
+
+//////////////METODOS PARA EL LOADER//////////////////
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	
